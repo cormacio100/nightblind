fa66173f26c64b278339e01fc58c814f0d409495
cormacio100 <cormac.music@gmail.com>
2016-12-22 17:58:37 +0000

    Further work on API Service

--- js/controller.js
+++ js/controller.js
@@ -4,21 +4,6 @@
 //	-	$scope - default
 //	-	$location - used for routing
 
-// allow for anchor links on the page
-/** NOT WORKING YET **/
-/*
-angular.module('AnchorLinkControllers',[])
-	.controller('AnchorController',function($scope,$location,$anchorScroll){
-		$scope.scrollTo = function(id){
-
-			console.log('scrolling');
-
-			$location.hash(id);
-			$anchorScroll;
-		}
-	});*/
-
-
 // Controller to build the NAVIGATION
 angular.module('SectionControllers',[])
 	.controller('NavController',function($scope,$location,$anchorScroll){
@@ -53,7 +38,7 @@ angular.module('SectionControllers',[])
 
 		// function retrieves data from service
 		var retrieveData = function(url,shortname){
-			
+
 			// call to API service
 			// pass the url. No params passed
 			MemberAPIService.retrieveMembers(url).then(function(results) {
@@ -87,133 +72,6 @@ angular.module('SectionControllers',[])
 
 
 	})
-
-
-	/*.controller('MemberController',function($scope,$location,MemberAPIService){
-		//console.log('Member Section loaded');	
-
-		
-		// function HIDES details of band members
-		var hideMembers = function(obj){
-			var name = obj.name;
-			var hideArr = [];
-
-			// build the relavant array for hiding divs
-			if('allMembers' == name){
-				hideArr=['cormac','hugh','davy','freek'];
-			}else if('cormac' == name){
-				hideArr=['hugh','davy','freek'];
-			}else if('hugh' == name){
-				hideArr=['cormac','davy','freek'];
-			}else if('davy' == name){
-				hideArr=['cormac','hugh','freek'];
-			}else if('freek' == name){
-				hideArr=['cormac','hugh','davy'];
-			}
-
-			// hide divs
-			$.each(hideArr,function(index,value){
-				var hideDiv = '#'+value+'_details';
-				$(hideDiv).hide();
-			});
-		};
-
-		var makeAPICall = function(obj){
-			// array to hold band members
-			//$scope.bandMemberArr = [];
-
-			// currently no API exists to pull data from
-			var url = '';
-
-			console.log('sending API request');
-
-			var member = {};
-
-			// pass URL and user obj that contains the requested name of band member
-			//return MemberAPIService.retrieveMembers(url, obj);
-			var member = MemberAPIService.retrieveMembers(url, obj);
-			console.log('returned member name is '+member.name);
-			return member;
-
-			/*console.log('returned member name is '+member.name);
-
-			$scope.title = member.title;
-			$scope.name = member.title;
-			$scope.instrument = member.title;
-			$scope.gear = member.title;*/
-
-			//console.log('member is '+member.name);
-
-			/*MemberAPIService.retrieveMembers(url, obj).then(function(results){
-
-				$scope.member = MemberAPIService.retrieveMembers(result);
-
-				// display the data
-				//$scope.member = results.data;
-			}).catch(function(err){
-				// display the error
-				console.log(err);
-			});
-		};
-
-
-		// by default all member divs are hidden
-		var obj = {
-			name:'allMembers'
-		};
-		hideMembers(obj);
-
-		var memberToShow = {};
-
-		$('#cormacThumb').click(function(){
-			
-			this.name='cormac';
-
-			console.log('name clicked: '+this.name);
-			// need to hide relevant divs for other members
-			//hideMembers(this);
-			//console.log('showing cormac div');
-			//$('#cormac_details').slideToggle();
-			memberToShow = makeAPICall(this);
-			/*MemberAPIService.retrieveMembers(url, obj);
-
-			console.log('returned member name is '+member.name);
-
-			$scope.title = member.title;
-			$scope.name = member.title;
-			$scope.instrument = member.title;
-			$scope.gear = member.title;
-		});
-
-		$('#hughThumb').click(function(){
-			this.name='hugh';
-			//hideMembers(this);
-			//$('#hugh_details').slideToggle();
-			memberToShow = makeAPICall(this);
-		});
-		$('#davyThumb').click(function(){
-			this.name='davy';
-			//hideMembers(this);
-			//$('#davy_details').slideToggle();
-			memberToShow = makeAPICall(this);
-		});
-		$('#freekThumb').click(function(){
-			this.name='freek';
-			//hideMembers(this);
-			//$('#freek_details').slideToggle();
-			memberToShow = makeAPICall(this);
-		});
-
-		console.log('returned member name is '+memberToShow.name);
-
-		$scope.title = memberToShow.title;
-		$scope.name = memberToShow.title;
-		$scope.instrument = memberToShow.title;
-		$scope.gear = memberToShow.title;
-
-
-
-	})*/
 	.controller('GalleryController',function($scope,$location){
 
 		$scope.showModal = false;
@@ -233,33 +91,3 @@ angular.module('SectionControllers',[])
 		};
 	
 	});
-
-
-
-
-/*
-angular.module('RouteControllers',[])
-
-	.controller('HomeController',function($scope,$location){
-
-	})
-	.controller('AboutController',function($scope,$location){
-
-	})
-	.controller('MemberController',function($scope,$location){
-		
-	})
-	.controller('GalleryController',function($scope,$location){
-
-
-		
-	})
-	.controller('MusicController',function($scope,$location){
-		
-	})
-	.controller('VideoController',function($scope,$location){
-		
-	})
-	.controller('ContactController-+',function($scope,$location){
-		
-	});*/
